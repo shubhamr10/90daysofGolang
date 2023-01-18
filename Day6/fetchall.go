55fdfe826a742ff989d28adf77f7d04072d123cc
shubhamr10 <shubhamrauniyar10@gmail.com>
2023-01-18 10:21:25 +0530

    Day 6 : fetching urls concurrently

---
+++ Day6/fetchall.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Fetchall fetches the URLs in parallel and reports their times and sizes.
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // starts a goroutine
+	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive a channel ch
+	}
+	fmt.Printf("%.2f seconds elapsed \n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(io.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2f seconds  %7d bytes  %s url", secs, nbytes, url)
+}
